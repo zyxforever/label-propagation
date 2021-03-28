2fb5cd69125ffd18229e10d02454a6d6a95704c8
zyxforever <384498377@163.com>
2021-03-28 10:45:48 +0800

    nothing

--- src/dataset.py
+++ src/dataset.py
@@ -1,23 +1,22 @@
 import scipy.io
-
+import numpy as np 
 class Dataset:
 
     def __init__(self,cfg):
         self.cfg=cfg  
 
     def load_dataset(self):
-        if self.cfg.data_set=='pen':
-            return self.load_data_mat()
-
+        return self.load_data_mat()
+        
     def load_data_mat(self,shuffle=True):
         # 读取特征矩阵
         fea = scipy.io.loadmat(self.cfg.dataset_path)
         # 获取特征矩阵 行 列大小
-        N = fea['fea'].shape[0]
-        M = fea['fea'].shape[1]
+        N = fea['data'].shape[0]
+        M = fea['data'].shape[1]
         # 随机抽样
         idx_rand = np.random.choice(N, size=N, replace=False)
-        X = fea['fea'][idx_rand] if shuffle else fea['fea']
+        X = fea['labels'][idx_rand] if shuffle else fea['fea']
         Y = fea['gt'][idx_rand] if shuffle else fea['gt']
         Y = np.squeeze(Y)
         return X, Y, N, M
--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
 
+import logging 
+import argparse 
+
+from algorithms.lp import lgc
+from dataset import Dataset
+
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s-%(name)s -%(levelname)s-%(message)s')
+logger=logging.getLogger(__name__)
+
 class Agent:
 
     def __init__(self):
-        self.cfg=config()
-        self.dataset=Dataset(self.cfg).load_dataset()
+        self.cfg=self.config()
+        self.data_set=Dataset(self.cfg).load_dataset()
         
     def config(self):
         parser = argparse.ArgumentParser(description='uncertainty')
-        parser.add_argument('--dataset_path',default='/home/zyx/datasets')
+        
         parser.add_argument('--model',default='cnn', choices=['cnn', 'dropout', 'scissors'])
+        parser.add_argument('--dataset_path',default='/home/zyx/datasets/MNIST10k.mat')
         parser.add_argument('--data_set',default='mnist')
+        parser.add_argument('--label_rate',default=0.05)
         parser.add_argument('--train_batch_size',default=128)
         parser.add_argument('--epoch',default=50)
         parser.add_argument('--learning_rate',default=1e-2)
@@ -17,7 +28,8 @@ class Agent:
         return parser.parse_args()
 
     def run(self):
-        
+        X,Y,N,M=self.data_set
+        lgc(X,Y)
 if __name__=='__main__':
     agent=Agent()
     agent.run()
