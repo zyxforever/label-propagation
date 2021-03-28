be6a8b0e73619df069bc57302b1e1d4e7385e77d
zyxforever <384498377@163.com>
2021-03-28 17:34:00 +0800

    nothing

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ class Agent:
         
         parser.add_argument('--model',default='cnn', choices=['cnn', 'dropout', 'scissors'])
         parser.add_argument('--dataset_path',default='/home/zyx/datasets/MNIST10k.mat')
-        parser.add_argument('--data_set',default='mnist')
+        parser.add_argument('--data_set',default='cora')
         parser.add_argument('--label_rate',default=0.05)
         parser.add_argument('--train_batch_size',default=128)
         parser.add_argument('--epoch',default=50)
@@ -28,8 +28,8 @@ class Agent:
         return parser.parse_args()
 
     def run(self):
-        X,Y,N,M=self.data_set
-        lgc(X,Y)
+        adj, features, labels, idx_train, idx_val, idx_test = self.data_set
+        
 if __name__=='__main__':
     agent=Agent()
     agent.run()
