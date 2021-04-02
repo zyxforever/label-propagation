c679735fda0e4e3ac348ae800b6315a69066d297
zyxforever <384498377@163.com>
2021-04-02 19:51:14 +0800

    nothing

---
+++ algorithms/label_propagation.py
@@ -0,0 +1,10 @@
+
+from abc import abstractmethod
+
+class LabelPropagation:
+    @abstractmethod
+    def lgc():
+        print("Hello World") 
+
+if __name__=='__main__':
+    LabelPropagation.lgc()
